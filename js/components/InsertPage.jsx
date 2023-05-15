b4890ea568164ab5ed11fd49ec46fe6adc901d13
negoitaariadna19 <negoitaariadna19@stud.ase.ro>
2023-05-15 21:40:34 +0300

    final

--- js/components/InsertPage.jsx
+++ js/components/InsertPage.jsx
@@ -43,7 +43,7 @@ export default function InsertPage() {
 					</div>
 					<div className="mb-6">
 						<label htmlFor="description"
-						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your fun fact</label>
+						       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
 						<textarea id="description"
 						          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						          required/>
