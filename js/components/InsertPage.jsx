fd0a79b6b38a630a81f9fe83b8e2fd9444093243
negoitaariadna19 <negoitaariadna19@stud.ase.ro>
2023-05-15 21:05:58 +0300

    final

--- js/components/InsertPage.jsx
+++ js/components/InsertPage.jsx
@@ -27,14 +27,16 @@ export default function InsertPage() {
 	}
 
 	return (
-		<section className={"bg-white"}>
+		<section className={"bg-[#ddd6fe]"}>
 			<div className={"container px-6 py-10 mx-auto"}>
-				<h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-blue-600"}>Insert your fun fact</h1>
-				<p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-blue-600"}>This is where you can insert your fun fact</p>
+				<h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-purple-600"}>Insert your favorite book</h1>
+				<p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-purple-600"}>This is where you can insert your book</p>
+				
+				
 
 				<form>
 					<div className="mb-6">
-						<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your title</label>
+						<label htmlFor="email" className="block mb-2 text-sm font-medium text--900 dark:text-white">Your title</label>
 						<input type="text" id="title"
 						       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						       placeholder="Your title" required/>
@@ -46,10 +48,19 @@ export default function InsertPage() {
 						          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						          required/>
 					</div>
-					<button type="submit"
-					        onClick={ insertRecord }
-					        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
-					</button>
+					<div className="flex justify-between mt-4">
+					<button
+                    type="submit"
+                    onClick={insertRecord}
+                    className="text-white bg-purple-500 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  mb-2 mr-2"
+                >
+                    Submit
+                </button>
+                <a href="/" className="text-white bg-purple-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-400 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 mr-2">
+                    Back
+                </a>
+                
+            </div>
 				</form>
 
 			</div>
--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -40,25 +40,28 @@ export default function MainPage() {
 	console.log(records);
 
 	return (
-		<section className={"bg-white"}>
+		<section className={"bg-[#ddd6fe]"}>
+	
 			<div className={"container px-6 py-10 mx-auto"}>
-				<h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-blue-600"}>Fun facts app</h1>
-				<p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-blue-600"}>This is an app that showcases fun facts</p>
-
+				<h1 className={"w-[500px] mx-auto text-center text-6xl font-bold text-purple-600"}>Book app</h1>
+				<p className={"w-[1000px] mx-auto text-center mt-4 text-3xl text-purple-600"}>This is an app that describes various books</p>
+				<a href="/insert" className="mt-4 text-gray-900 bg-purple-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-400 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+    Inserati o carte
+				</a>
 				<div className={"grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3"}>
 					{
 						records.map(record => (
 							<div key={record._id}
-								className={"max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow "}
+								className={"max-w-sm p-6 bg-[#fdf4ff] border-4 border-purple-400 rounded-lg shadow "}
 							>
-								<h3 className={"mb-2 text-2xl font-bold text-red-500"}>{record.titlu}</h3>
+								<h3 className={"mb-2 text-2xl font-bold text-violet-500"}>{record.titlu}</h3>
 								<p className={"font-normal "}>{record.descriere}</p>
 
 								<button type="button"
 								        onClick={deleteRecord}
 								        id={record._id}
-								        className="mt-4 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-									Delete!
+								        className="mt-4 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+									Delete
 								</button>
 							</div>
 						))
